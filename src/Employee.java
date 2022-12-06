82f2018fe9bce4e87609916854fac2b734f7f54f
max <kabinidi@mail.ru>
2022-12-06 18:44:23 +0300

    Ok

--- src/Employee.java
+++ src/Employee.java
@@ -7,66 +7,51 @@ public class Employee {
     int department;
     int salary;
     int id;
-
     private static int counter = 0;
-
-
-
-
-
-
-
-    public Employee (String surname,String name,String patronymic, int department,int salary){
+    public Employee(String surname, String name, String patronymic, int department, int salary) {
         this.name = name;
         this.surname = surname;
         this.patronymic = patronymic;
         this.department = department;
         this.salary = salary;
-        this.id= counter;
+        this.id = counter;
         counter++;
-
-
-
     }
-    public String getName(){
-        return name;
-    }
-    public String getSurname(){
-        return surname;
-    }
-    public String getPatronymic(){
-        return patronymic;
-    }
-    public int getDepartment(){
-        return department;
-    }
-    public int getSalary(){
-        return salary;
-    }
-    public  int getCounter(){
-        return counter;
-
-    }
-
-
-
-
-
-
-
     public void setDepartment(int department){
         this.department = department;
     }
     public void setSalary(int salary){
         this.salary = salary;
     }
-
-
-
-
-
-
-
-
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public String getPatronymic() {
+        return patronymic;
+    }
+    public int getDepartment() {
+        return department;
+    }
+    public int getSalary() {
+        return salary;
+    }
+    public int getCounter() {
+        return counter;
+    }
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                ", id=" + id +
+                '}';
+    }
 }
+
 
